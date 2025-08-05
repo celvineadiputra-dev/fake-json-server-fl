a65539cfe6cecca8346552f87061837cfb17d00c
celvine <celvineadiputra2@gmail.com>
2025-08-05 13:04:51 +0700

    Init : [Svelte Example]

---
+++ example-svelte/src/routes/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import LoginForm from '$lib/components/app/login-form.svelte';
+</script>
+
+<svelte:head>
+	<title>Sign In</title>
+</svelte:head>
+
+<div class="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+	<div class="w-full max-w-sm md:max-w-3xl">
+		<LoginForm />
+	</div>
+</div>
---
+++ example-svelte/src/routes/sign-up/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import SignUpForm from '@/components/app/sign-up-form.svelte';
+</script>
+
+<svelte:head>
+	<title>Sign Up</title>
+</svelte:head>
+
+<div class="flex min-h-svh flex-col items-center justify-center bg-muted p-6 md:p-10">
+	<div class="w-full max-w-sm md:max-w-3xl">
+		<SignUpForm />
+	</div>
+</div>
